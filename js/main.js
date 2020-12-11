9f0c69319f1edb7299491f922cc49092d59404a5
Ghassan ALNomani <alnomanif15@gmail.com>
2020-12-11 23:01:42 +0300

    edit main.js add 8 conditions for winners

--- js/main.js
+++ js/main.js
@@ -16,6 +16,9 @@ function setMessage(msg){
     $('.boardInst').text(msg);
 }
 
+
+
+
 // Start move
 function move(spot){ 
     if(spot.innerText == ""){
@@ -27,16 +30,74 @@ function move(spot){
 }
 
 function switchTurn(){
-    if(turn == "X"){
+    if(checkWinner()){
+        alert("win");
+    }else if(turn == "X"){
         turn = "O";
+        setMessage("It's " + turn + " Your turn");
     }else{
         turn = "X";
+        setMessage("It's " + turn + " Your turn");
     }
+    
+}
 
-    setMessage("It's " + turn + " Your turn");
+// create funtion to give me spot
+
+function giveMeSpot(num){
+    return $('#spot' + num).text();
 }
 
 
-
-
+// create function checkWinner
+var Xsign = "X";
+var Osign = "O";
+function checkWinner(){
+    var result = false;
+    var rowOneOfOne = 1;
+    var rowOneOfTwo = 2;
+    var rowOneOfThree = 3;
+    var rowTwoOfOne = 4;
+    var rowTwoOfTwo = 5;
+    var rowTwoOfThree = 6;
+    var rowThreeOfOne = 7;
+    var rowThreeOfTwo = 8;
+    var rowThreeOfThree = 9;
+    if(giveMeSpot(rowOneOfOne)&&giveMeSpot(rowOneOfTwo)&&giveMeSpot(rowOneOfThree) == Xsign){
+        result = true;
+    }else if(giveMeSpot(rowTwoOfOne)&&giveMeSpot(rowTwoOfOne)&&giveMeSpot(rowTwoOfThree) == Xsign){
+        result = true;
+    }else if(giveMeSpot(rowThreeOfOne)&&giveMeSpot(rowThreeOfTwo)&&giveMeSpot(rowThreeOfThree) == Xsign){
+        result = true;
+    }else if(giveMeSpot(rowOneOfOne)&&giveMeSpot(rowTwoOfTwo)&&giveMeSpot(rowTwoOfThree) == Xsign){
+        result = true;
+    }else if(giveMeSpot(rowOneOfTwo)&&giveMeSpot(rowTwoOfTwo)&&giveMeSpot(rowThreeOfTwo) == Xsign){
+        result = true;
+    }else if(giveMeSpot(rowOneOfThree)&&giveMeSpot(rowTwoOfThree)&&giveMeSpot(rowThreeOfThree) == Xsign){
+        result = true;
+    }else if(giveMeSpot(rowOneOfOne)&&giveMeSpot(rowTwoOfTwo)&&giveMeSpot(rowThreeOfThree) == Xsign){
+        result = true;
+    }else if(giveMeSpot(rowOneOfThree)&&giveMeSpot(rowTwoOfTwo)&&giveMeSpot(rowThreeOfOne) == Xsign){
+        result = true;
+    }else if(giveMeSpot(rowOneOfOne)&&giveMeSpot(rowOneOfTwo)&&giveMeSpot(rowOneOfThree) == Osign){
+        result = true;
+    }else if(giveMeSpot(rowTwoOfOne)&&giveMeSpot(rowTwoOfOne)&&giveMeSpot(rowTwoOfThree) == Osign){
+        result = true;
+    }else if(giveMeSpot(rowThreeOfOne)&&giveMeSpot(rowThreeOfTwo)&&giveMeSpot(rowThreeOfThree) == Osign){
+        result = true;
+    }else if(giveMeSpot(rowOneOfOne)&&giveMeSpot(rowTwoOfTwo)&&giveMeSpot(rowTwoOfThree) == Osign){
+        result = true;
+    }else if(giveMeSpot(rowOneOfTwo)&&giveMeSpot(rowTwoOfTwo)&&giveMeSpot(rowThreeOfTwo) == Osign){
+        result = true;
+    }else if(giveMeSpot(rowOneOfThree)&&giveMeSpot(rowTwoOfThree)&&giveMeSpot(rowThreeOfThree) == Osign){
+        result = true;
+    }else if(giveMeSpot(rowOneOfOne)&&giveMeSpot(rowTwoOfTwo)&&giveMeSpot(rowThreeOfThree) == Osign){
+        result = true;
+    }else if(giveMeSpot(rowOneOfThree)&&giveMeSpot(rowTwoOfTwo)&&giveMeSpot(rowThreeOfOne) == Osign){
+        result = true;
+    }else{
+        //alert("Tie game");
+    }
+    return result;
+}
 
