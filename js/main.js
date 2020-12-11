f6f68cbc32b769a41dfcd3b8611cf80e91414604
Ghassan ALNomani <alnomanif15@gmail.com>
2020-12-11 13:34:58 +0300

    edit main.js add function call move and add switchTurn function to switch between x and o

--- js/main.js
+++ js/main.js
@@ -1,5 +1,36 @@
 // Function navigate to another page
-$('#butStart').on('click', () =>{
+$('#butGame').on('click', () =>{
     location.href = "game.html"
 })
+
+
+// Start function  Start Game
+
+var turn = "X";
+
+$('#butStart').on('click', ()=>{
+    setMessage(turn + " start game")
+})
+
+function setMessage(msg){
+    $('.boardInst').text(msg);
+}
+
+// Start move
+function move(spot){ 
+    spot.innerText = turn;
+    switchTurn();
+}
+
+function switchTurn(){
+    if(turn == "X"){
+        turn = "O";
+    }else{
+        turn = "X";
+    }
+}
+
+
+
+
 
