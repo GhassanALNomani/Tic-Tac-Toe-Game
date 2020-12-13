fed3e0e60d49c60121c3a660421ecb437af896a8
Ghassan ALNomani <alnomanif15@gmail.com>
2020-12-13 10:06:56 +0300

    edit

--- js/main.js
+++ js/main.js
@@ -47,9 +47,9 @@ function giveMeSpot(num){
 
 //create function checkWinner
 
-function Winner(spotOne, spotTwo, spotThree, move){
+function Winner(spotOne, spotTwo, spotThree, spotFour, move){
     var result = false;
-    if(giveMeSpot(spotOne) == move && giveMeSpot(spotTwo) == move && giveMeSpot(spotThree) == move){
+    if(giveMeSpot(spotOne) == move && giveMeSpot(spotTwo) == move && giveMeSpot(spotThree) == move && giveMeSpot(spotFour) == move){
         result = true;
     }
 
@@ -58,14 +58,25 @@ function Winner(spotOne, spotTwo, spotThree, move){
 
 function checkWinner(move){
     var result = false
-    if( Winner(1,2,3,move)||
-        Winner(4,5,6,move)||
-        Winner(7,8,9,move)||
-        Winner(1,5,9,move)||
-        Winner(3,5,7,move)||
-        Winner(1,4,7,move)||
-        Winner(2,5,8,move)||
-        Winner(3,6,9,move)){
+    if( Winner(1,2,3,4, move)||
+        Winner(7,8,9,10,move)||
+        Winner(13,14,15,16,move)||
+        Winner(19,20,21,22,move)||
+        Winner(25,26,27,28,move)||
+        Winner(31,32,33,34,move)||
+        Winner(3,4,5,6,move)||
+        Winner(9,10,11,12,move)||
+        Winner(15,16,17,18,move)||
+        Winner(21,22,23,24,move)||
+        Winner(27,28,29,30,move)||
+        Winner(33,34,35,36,move)||
+        Winner(2,3,4,5,move)||
+        Winner(8,9,10,11,move)||
+        Winner(14,15,16,17,move)||
+        Winner(20,21,22,23,move)||
+        Winner(26,27,28,29,move)||
+        Winner(32,33,34,35,move)||
+        Winner(2,8,14,20,move)){
             result = true;
         }
     return result;
@@ -87,3 +98,9 @@ $('#restart').on('click', function(){
     $('.boardInst').text("");
 });
 
+
+//Create button  for when i clicked quit game
+
+$('#quit').on('click', function(){
+    window.close()
+})
