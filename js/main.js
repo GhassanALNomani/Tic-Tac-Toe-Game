38e6ce2fe37718699cf9c55b4e6e2a28fca8d93e
Ghassan ALNomani <alnomanif15@gmail.com>
2020-12-13 14:00:06 +0300

    edit

--- js/main.js
+++ js/main.js
@@ -25,10 +25,11 @@ function move(spot){
 }
 // function switch turn between player one and two
 function switchTurn(){
-    if(checkWinner(turn)){
-        alert("Win!")
-    }
-    if(turn == "X"){
+    if(checkWinner("X")){
+        alert("Win! Player One")
+    }else if(checkWinner("O")){
+        alert("Win! Player Two")
+    }else if(turn == "X"){
         turn = "O";
         setMessage("Player Two turn" );
     }else{
@@ -47,9 +48,9 @@ function giveMeSpot(num){
 
 //create function checkWinner
 
-function Winner(spotOne, spotTwo, spotThree, spotFour, move){
+function Winner(spotOne, spotTwo, spotThree, move){
     var result = false;
-    if(giveMeSpot(spotOne) == move && giveMeSpot(spotTwo) == move && giveMeSpot(spotThree) == move && giveMeSpot(spotFour) == move){
+    if(giveMeSpot(spotOne) == move && giveMeSpot(spotTwo) == move && giveMeSpot(spotThree) == move){
         result = true;
     }
 
@@ -58,25 +59,9 @@ function Winner(spotOne, spotTwo, spotThree, spotFour, move){
 
 function checkWinner(move){
     var result = false
-    if( Winner(1,2,3,4, move)||
-        Winner(7,8,9,10,move)||
-        Winner(13,14,15,16,move)||
-        Winner(19,20,21,22,move)||
-        Winner(25,26,27,28,move)||
-        Winner(31,32,33,34,move)||
-        Winner(3,4,5,6,move)||
-        Winner(9,10,11,12,move)||
-        Winner(15,16,17,18,move)||
-        Winner(21,22,23,24,move)||
-        Winner(27,28,29,30,move)||
-        Winner(33,34,35,36,move)||
-        Winner(2,3,4,5,move)||
-        Winner(8,9,10,11,move)||
-        Winner(14,15,16,17,move)||
-        Winner(20,21,22,23,move)||
-        Winner(26,27,28,29,move)||
-        Winner(32,33,34,35,move)||
-        Winner(2,8,14,20,move)){
+    if( Winner(1,2,3,move)|| Winner(4,5,6,move)|| Winner(7,8,9,move)||   //in row "wins"
+        Winner(1,5,9,move)|| Winner(3,5,7,move)||                        //in diagonal "wins"   
+        Winner(1,4,7,move)|| Winner(2,5,8,move)|| Winner(3,6,9,move)){   // in column "wins"
             result = true;
         }
     return result;
@@ -94,6 +79,7 @@ $("#menu").on('click', function(){
 //Create event for button when i clicked restart game
 //check
 $('#restart').on('click', function(){
+    turn = "X";
     $('#container .container-game button').text("");
     $('.boardInst').text("");
 });
